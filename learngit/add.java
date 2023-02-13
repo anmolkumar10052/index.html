32e6f8db371b19d768bd5f2bc8c70025dcbaaf43
Anmol kumar <anmolkumar10052@gmail.com>
2023-02-13 19:15:33 +0530

    initial commit

---
+++ learngit/add.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+import javax.sound.sampled.SourceDataLine;
+public class add{
+    public static void main(String[] args) {
+        int a, b,sum;
+        System.out.println("enter 1st number");
+        Scanner sc = new Scanner(System.in);
+        a=sc.nextInt();
+        System.out.println("enter 2nd number");
+        b=sc.nextInt();
+        System.out.println("the sum of two number is = " + (a+b));
+    }
+    
+    
+}
